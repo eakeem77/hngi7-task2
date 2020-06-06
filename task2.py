5fc2b24bec92edad395e489a35f126945cecfb75
eakeem77 <66512993+eakeem77@users.noreply.github.com>
2020-06-06 11:23:38 +0100

    created task 2

---
+++ task2.py
@@ -0,0 +1,6 @@
+name = "Eniodunmo Gbolahan Akeem"
+hng_id = "HNG-05755"
+email = "eakeem77@gmail.com"
+language = "python"
+
+print(f"Hello World, this is {name} with HNG ID {hng_id} using {language} for stage 2 task email {email}")
